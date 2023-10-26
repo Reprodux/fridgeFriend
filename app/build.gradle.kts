a7d3ef79991dce032fc29daec7d9d0f56d50ad81
Reprodux <ha.briann@gmail.com>
2023-10-26 17:23:17 -0400

    enabled camera and internet access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
