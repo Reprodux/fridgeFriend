946736c378e074e6e37125d354a665dc85b9436c
Reprodux <ha.briann@gmail.com>
2023-10-26 00:24:11 -0400

    added working prototype login page and sign out

--- app/src/main/java/com/example/fridge_friend/MainActivity.java
+++ app/src/main/java/com/example/fridge_friend/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.fridge_friend;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkData(View V){
+        FirebaseAuth.getInstance().signOut();
+        Log.i(TAG, "Signed out");
+        Toast.makeText(MainActivity.this, "Signed out",
+                Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
