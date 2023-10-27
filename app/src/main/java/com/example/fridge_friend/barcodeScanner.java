2fdf135bd544522f37aa9d6e72d2ceec9d8fbb93
Reprodux <ha.briann@gmail.com>
2023-10-27 12:23:38 -0400

    added an activityResultLauncher for scanoptions

--- app/src/main/java/com/example/fridge_friend/barcodeScanner.java
+++ app/src/main/java/com/example/fridge_friend/barcodeScanner.java
@@ -1,5 +1,6 @@
 package com.example.fridge_friend;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +26,8 @@ import com.google.mlkit.vision.barcode.ZoomSuggestionOptions;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CaptureActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.io.IOException;
 
@@ -31,19 +35,39 @@ public class barcodeScanner extends AppCompatActivity {
 
     private SurfaceView surfaceView;
     private BarcodeDetector barcodeDetector;
+    private Button start_scan_btn;
     private CameraSource cam;
     private static final int REQUEST_CAM_CODE = 201;
     private TextView barcode_txt;
     private String barcode_data;
 
-
+    private final ActivityResultLauncher<ScanOptions> barcode_scanner = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null){
+                    Toast.makeText(barcodeScanner.this, "Cancelled", Toast.LENGTH_LONG).show();
+                        } else {
+                    Toast.makeText(barcodeScanner.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                    barcode_txt = findViewById(R.id.barcode_text);
+                    barcode_txt.setText(result.getContents());
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
         barcode_txt = findViewById(R.id.barcode_text);
+        start_scan_btn = findViewById(R.id.start_scan_btn);
+
+        start_scan_btn.setOnClickListener((view -> {
+            ScanOptions options = new ScanOptions();
+            options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES);
+            options.setBeepEnabled(true);
+            barcode_scanner.launch(new ScanOptions());
+            //startBarcodeScanner();
+        }));
 
     }
+    /* USES DEPRECIATED METHOD OF BARCODE SCANNER, unsure if this one works lol
 
     public void startBarcodeScanner(){
         Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
@@ -72,7 +96,7 @@ public class barcodeScanner extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "No results", Toast.LENGTH_SHORT).show();
         }
     }
-
+    */
     @Override
     protected void onPause() {
         super.onPause();
