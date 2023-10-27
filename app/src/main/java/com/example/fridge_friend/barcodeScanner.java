72eeead098007f9f3ba3b354dc0a441f88ef9d94
Reprodux <ha.briann@gmail.com>
2023-10-27 12:09:54 -0400

    overhauled barcode scanner to use zxing library, but current function is depreciated

--- app/src/main/java/com/example/fridge_friend/barcodeScanner.java
+++ app/src/main/java/com/example/fridge_friend/barcodeScanner.java
@@ -1,8 +1,10 @@
 package com.example.fridge_friend;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.SparseArray;
@@ -15,14 +17,17 @@ import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.barcode.ZoomSuggestionOptions;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
 
 import java.io.IOException;
 
 public class barcodeScanner extends AppCompatActivity {
-
-
 
     private SurfaceView surfaceView;
     private BarcodeDetector barcodeDetector;
@@ -31,115 +36,52 @@ public class barcodeScanner extends AppCompatActivity {
     private TextView barcode_txt;
     private String barcode_data;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
-        surfaceView = findViewById(R.id.surface_view);
         barcode_txt = findViewById(R.id.barcode_text);
-        initialiseDetectorsAndSources();
 
     }
 
-    private void startBarcodeScanner(){
+    public void startBarcodeScanner(){
         Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
         BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
                 .setBarcodeFormats(
                     Barcode.ALL_FORMATS
                 ).build();
+        //starts the detector
+        IntentIntegrator integrator = new IntentIntegrator(barcodeScanner.this);
+        integrator.setCaptureActivity(CaptureActivity.class);
+        integrator.setOrientationLocked(true);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.initiateScan();
 
     }
-    private void initialiseDetectorsAndSources() {
 
-        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
-
-        barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.ALL_FORMATS)
-                .build();
-
-        cam= new CameraSource.Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1920, 1080)
-                .setAutoFocusEnabled(true) //you should add this feature
-                .build();
-
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    if (ActivityCompat.checkSelfPermission(barcodeScanner.this,android.Manifest.permission.CAMERA ) == PackageManager.PERMISSION_GRANTED) {
-                        cam.start(surfaceView.getHolder());
-                    } else {
-                        ActivityCompat.requestPermissions(barcodeScanner.this, new
-                                String[]{android.Manifest.permission.CAMERA}, REQUEST_CAM_CODE);
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cam.stop();
-            }
-        });
-
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-                if (barcodes.size() != 0) {
-
-
-                    barcode_txt.post(new Runnable() {
-
-                        @Override
-                        public void run() {
-
-                            if (barcodes.valueAt(0).email != null) {
-                                barcode_txt.removeCallbacks(null);
-                                barcode_data = barcodes.valueAt(0).email.address;
-                                barcode_txt.setText(barcode_data);
-
-                            } else {
-
-                                barcode_data = barcodes.valueAt(0).displayValue;
-                                barcode_txt.setText(barcode_data);
-
-
-                            }
-                        }
-                    });
-
-                }
-            }
-        });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            Toast.makeText(getApplicationContext(), result.getContents(), Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "No results", Toast.LENGTH_SHORT).show();
+        }
     }
-
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        cam.release();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        initialiseDetectorsAndSources();
     }
 }
