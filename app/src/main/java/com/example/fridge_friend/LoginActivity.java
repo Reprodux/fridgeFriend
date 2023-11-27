f694b2963ce20b55f920d1d7f65deff2c4c5946c
Sterling Fullerton <sterlingfullerton@me.com>
2023-11-27 13:34:14 -0500

    Forced Lightmode (Dark mode is too much work to implement rn)

--- app/src/main/java/com/example/fridge_friend/LoginActivity.java
+++ app/src/main/java/com/example/fridge_friend/LoginActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -61,6 +62,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
