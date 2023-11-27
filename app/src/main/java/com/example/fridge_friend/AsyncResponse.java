1f4a60957eea52da0c1f5b0e111a8d025eb8ae54
Sterling Fullerton <sterlingfullerton@me.com>
2023-11-27 12:56:39 -0500

    Added data return from barcode_data_retrieval.java to barcodeScanner.java

--- app/src/main/java/com/example/fridge_friend/AsyncResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.fridge_friend;
-
-public interface AsyncResponse {
-    void processFinish(String product_valid, String product_name, String product_code, String product_categories, String brands, String product_facts);
-}
